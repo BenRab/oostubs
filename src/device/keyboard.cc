8370a4bf4431e7660ec45022f769480fda6039d4
christiansh <evibu@vollbio.de>
2013-06-02 18:03:06 +0200

    Teil C, keyboard.cc

--- src/device/keyboard.cc
+++ src/device/keyboard.cc
@@ -17,6 +17,7 @@
 
 extern PIC pic;
 extern Plugbox plugbox;
+extern unsigned int globalTaskChoice;
 
 /** \todo \~german implementieren \~english write implementation */
 Keyboard::Keyboard(){
@@ -35,18 +36,29 @@ void Keyboard::trigger(){
   unsigned short x;
   unsigned short y;
 
+  //Strg+Alt+Entf -> Neustart
   if(k.valid() && k.alt() && k.ctrl() && k.scancode() == 0x53)//k.scan.del)
   {
     reboot();
-  } 
+  }
+  //Alt+1 -> globalTaskChoice = 1 
+  else if(k.valid() && k.alt() && k.scancode() == 2)
+  {
+    globalTaskChoice = 1;
+  }
+  //Alt+2 -> globalTaskChoice = 2
+  else if(k.valid() && k.alt() && k.scancode() == 3)
+  {
+    globalTaskChoice = 2;
+  }
   else if(k.valid())
   {
-    kout.flush();
+   /* kout.flush();
     kout.getpos(x, y);
     kout.setpos(0, 10);
     kout << k;
     kout.flush();
-    kout.setpos(x, y);
+    kout.setpos(x, y);*/
   }
 }
 
